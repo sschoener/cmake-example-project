91dc6f9a67520677d154b1299f5766e182be7338
Sebastian Schöner <sebastian.schoener@gmail.com>
2019-08-27 21:53:41 +0200

    Remove C++11 warning in test code

--- myLibTest/minunit.h
+++ myLibTest/minunit.h
@@ -1,5 +1,5 @@
 #pragma once
 
 #define mu_assert(message, test) do { if (!(test)) return message; } while (0)
-#define mu_run_test(test) do { char *message = test(); tests_run++; if (message) return message; } while (0)
+#define mu_run_test(test) do { const char *message = test(); tests_run++; if (message) return message; } while (0)
 extern int tests_run;
